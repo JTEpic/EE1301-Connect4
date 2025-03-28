10407ebdc7883e79417e3b0b003c5e0a96d11555
JTEpic <jjrocks2006@att.net>
2025-03-27 19:48:06 -0500

    3.27.25.2

--- src/EE1301-Connect4.cpp
+++ src/EE1301-Connect4.cpp
@@ -18,10 +18,7 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
-
-  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
-  // Log.info("Sending Hello World to the cloud!");
-  // Particle.publish("Hello world!");
-  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+  // Log.info("Sending Hello World to the cloud");
+  // Particle.publish("Hello world");
+  // delay( 10 * 1000 ); // milliseconds and blocking
 }
